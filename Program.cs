caa0eaa5ae2aff43117fe6864b476fd0c1487041
Simon Domfrocht <simonbussetti01@gmail.com>
2026-05-19 16:39:45 -0300

    calculadora v1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+Console.WriteLine("Hello, World!");
 
 // int a;
 // int b;
@@ -8,31 +10,85 @@
 // Console.WriteLine("Valor de b: "+b);
 
 //Ejercicio 1
-Console.WriteLine("Ingrese numero mayor a 0: "); 
-string? texto = Console.ReadLine();
+// Console.WriteLine("Ingrese numero mayor a 0: "); 
+// string? texto = Console.ReadLine();
 
-int numero;
+// int numero;
 
-if(int.TryParse(texto, out numero))
+// if(int.TryParse(texto, out numero))
+// {
+//     if(numero > 0)
+//     {
+//         Console.WriteLine("Numero antes de ser invertido: "+ numero);
+//         Console.WriteLine("Numero invertido: ");
+
+//         while(numero > 0)
+//         {
+//             int aux = numero % 10;
+//             Console.Write(aux);
+//             numero = numero / 10;
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("El numero no es mayor a 0.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Lo que ingresaste no es un numero");
+// }
+
+
+//Ejercicio 2
+
+Console.WriteLine("Ingrese primer numero: "); 
+string? texto1 = Console.ReadLine();
+
+Console.WriteLine("Ingrese segundo numero: "); 
+string? texto2 = Console.ReadLine();
+
+double num1; 
+double num2;
+
+if(double.TryParse(texto1, out num1) && double.TryParse(texto2, out num2))
 {
-    if(numero > 0)
-    {
-        Console.WriteLine("Numero antes de ser invertido: "+ numero);
-        Console.WriteLine("Numero invertido: ");
+    Console.WriteLine("Primer Numero: "+ num1);
+    Console.WriteLine("Segundo Numero: "+ num2);
 
-        while(numero > 0)
+    Console.WriteLine("Ingrese numero de la operacion a hacer(1-Suma 2-Restar 3-Dividir 4-Multiplicar): "); 
+    string? texto3 = Console.ReadLine();
+    int operacion;
+    double resultado;
+
+    if(int.TryParse(texto3, out operacion) && (operacion > 0 || operacion < 5))
+    {
+        switch (operacion)
         {
-            int aux = numero % 10;
-            Console.Write(aux);
-            numero = numero / 10;
+            case 1:
+                resultado = num1 + num2;
+                Console.WriteLine("Resultado de la suma: "+ resultado);
+            break;
+            case 2:
+                resultado = num1 - num2;
+                Console.WriteLine("Resultado de la resta: "+ resultado);
+            break;
+            case 3:
+                resultado = num1 / num2;
+                Console.WriteLine("Resultado de la division: "+ resultado);
+            break;
+            case 4:
+                resultado = num1 * num2;
+                Console.WriteLine("Resultado de la multiplicacion: "+ resultado);
+            break;
         }
     }
     else
     {
-        Console.WriteLine("El numero no es mayor a 0.");
+        Console.WriteLine("El dato ingresado de la operacion no es valido");
     }
 }
 else
 {
-    Console.WriteLine("Lo que ingresaste no es un numero");
+    Console.WriteLine("Alguno de los datos ingresados no es un numero.");
 }
